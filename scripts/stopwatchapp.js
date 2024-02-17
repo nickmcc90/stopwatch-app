9a924ad4816edb5788bb1918cbc7a198e81c9f04
Nick <133172211+nickmcc90@users.noreply.github.com>
2024-02-17 13:43:41 -0500

    2/17

--- scripts/stopwatchapp.js
+++ scripts/stopwatchapp.js
@@ -18,6 +18,10 @@ document.querySelector('.js-reset-button').addEventListener('click', () => {
   reset();
 })
 
+document.querySelector('.js-timer-button').addEventListener('click', () => {
+  timerScreenSelect();
+})
+
 function correctTime() {
   const sButtonElement = document.querySelector('.js-stop-start-button');
   if(sButtonElement.innerHTML === 'Start') {
@@ -34,8 +38,7 @@ function stopwatch() {
   if(!isTimerOn) {
     isTimerOn = true;
     codeId = setInterval(() => {
-      time += 1;
-      updateTime();
+      stopwatchAlgorithm();
     }, 1000);
   } else {
     isTimerOn = false;
@@ -45,15 +48,65 @@ function stopwatch() {
   localStorage.setItem('time',JSON.stringify(time));
 }
 
-function reset() {
-  localStorage.removeItem('time');
-  time = '00:00';
+function stopwatchAlgorithm() {
+  time.onesSeconds += 1;
+  if(time.onesSeconds === 10) {
+    time.onesSeconds = 0;
+    time.tensSeconds += 1;
+  }
+  if(time.tensSeconds === 6) {
+    time.tensSeconds = 0;
+    time.onesMinutes = 1;
+  }
+  if(time.onesMinutes === 10) {
+    time.onesMinutes = 0;
+    time.tensMinutes = 1;
+  }
+  if(time.tensMinutes === 6) {
+    reset();
+  }
   updateTime();
-  console.log(time);
 }
+
+function reset() {
+  if(document.querySelector('.js-stop-start-button').innerHTML = 'Stop') {    // If we hit reset while the timer is still going...
+    isTimerOn = true;                                                         // we can switch the start text back and stop the counting.
+    stopwatch();
+    isTimerOn = false;
+  }
+  time = {
+    onesSeconds: 0,
+    tensSeconds: 0,
+    onesMinutes: 0,
+    tensMinutes: 0
+  };
+  localStorage.removeItem('time');        // this needs to be the last line.
+  updateTime();
+  console.log(time);    // debugger
+}
+
+
+function timerScreenSelect() {
+  const selectScreenElement = document.querySelector('.js-fixed-timer-select');
+  let htmlSelectScreen = `
+  <div>
+  
+  
+  
+  
+  `;
+
+  selectScreenElement.innerHTML = htmlSelectScreen;
+}
+
+
 
 function updateTime() {
   // time is going to be an object, made of 4 properties.
+  document.querySelector('.js-tens-minutes').innerHTML = time.tensMinutes;
+  document.querySelector('.js-ones-minutes').innerHTML = time.onesMinutes;
+  document.querySelector('.js-tens-seconds').innerHTML = time.tensSeconds;
+  document.querySelector('.js-ones-seconds').innerHTML = time.onesSeconds;
 }
 
 /* secret layout change code prep...
